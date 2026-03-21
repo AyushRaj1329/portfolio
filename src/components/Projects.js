ff5e48846a5503d090c6a09a07d7cbe57f2c0386
Ayush raj <ayushraj790673@gmail.com>
2026-03-22 04:11:34 +0530

    fix the project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,43 +3,75 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import ochi from "../assets/img/ochi.png";
+import rps from "../assets/img/rps.png";
+import ttt from "../assets/img/ttt.png";
+import currconv from "../assets/img/currconv.png";
+
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
 
-  const projects = [
+  const personal = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
+      title: "Sorting-Visualiser",
       description: "Design & Development",
       imgUrl: projImg2,
+      projurl: "https://github.com/AyushRaj1329/SFML-Sorting-Visualiser/",
     },
+
+
+  ];
+
+  const freelance = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
+      title: "Card-Sequence Validator",
       description: "Design & Development",
       imgUrl: projImg1,
+      projurl: "https://github.com/AyushRaj1329/Dual-Head-Card-Sequence-Validator/",
     },
+
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
+      title: "OMP-Portfolio",
       description: "Design & Development",
       imgUrl: projImg3,
+      projurl: "https://github.com/AyushRaj1329/OMP-Portfolio-website/",
     },
+
+  ];
+
+  const learning = [
+    {
+      title: "Frontend Clone",
+      description: "React Understanding",
+      imgUrl: ochi,
+      projurl: "https://ochi-website-clone-seven.vercel.app/",
+    },
+
+    {
+      title: "Currency-Converter",
+      description: "API Understanding",
+      imgUrl: currconv,
+      projurl: "https://ayushraj1329.github.io/Currency-Converter/",
+    },
+
+    {
+      title: "Stone Paper Scissors",
+      description: "JavaScript Understanding",
+      imgUrl: rps,
+      projurl: "https://ayushraj1329.github.io/stone-paper-scissors-/",
+    },
+
+    {
+      title: "Tic Tac Toe",
+      description: "JavaScript Understanding",
+      imgUrl: ttt,
+      projurl: "https://ayushraj1329.github.io/tic-tac-toe/",
+    },
+
   ];
 
   return (
@@ -49,45 +81,76 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>A collection of projects that showcase my skills in software development, problem-solving, and building real-world applications. From interactive visual tools to industry-focused systems, each project reflects my ability to design, develop, and deliver efficient solutions.</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Personal Projects</Nav.Link>
+                      </Nav.Item>
+
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Freelance Projects</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Learning</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {
+                            personal.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
                                 />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                              )
+                            })
+                          }
+                        </Row>
+
+                      </Tab.Pane>
+
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {
+                            freelance.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                />
+                              )
+                            })
+                          }
+                        </Row>
+
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {
+                            learning.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                />
+                              )
+                            })
+                          }
+                        </Row>
+
+                      </Tab.Pane>
+
+
+
+
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
